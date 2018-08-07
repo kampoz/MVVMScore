8e1be3175915383b38edfdfc21a5d96f416155ef
Kamil Poznakowski <kamil.poznakowski@embiq.com>
2018-08-07 11:33:55 +0200

    LiveData

--- app/src/main/java/com/example/programmer/mvvmscore/MainActivity.java
+++ app/src/main/java/com/example/programmer/mvvmscore/MainActivity.java
@@ -4,6 +4,8 @@ import android.arch.lifecycle.ViewModelProviders;
 import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -27,26 +29,37 @@ public class MainActivity extends AppCompatActivity {
         ActivityMainBinding activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         scoreViewModel = ViewModelProviders.of(this).get(ScoreViewModel.class);
         activityMainBinding.setViewmodel(scoreViewModel);
-//        activityMainBinding.setViewmodel(new ScoreViewModel());
+        activityMainBinding.setLifecycleOwner(this);
 
         btnAddOne = findViewById(R.id.btn_PLusOne);
         tvCounter = findViewById(R.id.tvCounter);
-                et = findViewById(R.id.et);
-                btnAddOne.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-//                addOneForTeamA(v);
-//                scoreViewModel.counter = String.valueOf(Integer.parseInt(scoreViewModel.counter + 1));
-                        scoreViewModel.counter = et.getText().toString();
-                Log.d("ViewModel", scoreViewModel.counter);
-//                tvCounter.setText(scoreViewModel.counter);
+        et = findViewById(R.id.et);
+
+
+        btnAddOne.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("LiveData", scoreViewModel.getScore().getValue());
+            }
+        });
+
+        et.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                scoreViewModel.counter.setValue(s.toString());
             }
         });
 
     }
-
-    public void addOneForTeamA(View v) {
-        scoreViewModel.counter = String.valueOf(Integer.parseInt(scoreViewModel.counter + 1));
-//        displayForTeamA(scoreViewModel.counter);
-    }
 }
+
--- app/src/main/java/com/example/programmer/mvvmscore/ScoreViewModel.java
+++ app/src/main/java/com/example/programmer/mvvmscore/ScoreViewModel.java
@@ -1,5 +1,7 @@
 package com.example.programmer.mvvmscore;
 
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.MutableLiveData;
 import android.arch.lifecycle.ViewModel;
 
 /**
@@ -7,6 +9,15 @@ import android.arch.lifecycle.ViewModel;
  */
 public class ScoreViewModel extends ViewModel {
 
-    public String counter = String.valueOf(0);
+//    public String counter = String.valueOf(0);
+
+    public MutableLiveData<String> counter = new MutableLiveData<>();
+
+    public MutableLiveData<String> getScore(){
+        if (counter == null) {
+            counter = new MutableLiveData<String>();
+        }
+        return counter;
+    }
 
 }
